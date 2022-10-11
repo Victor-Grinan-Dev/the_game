a4ee5ea2ce1b8854d168ede40128a619e5c9b34c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-11 18:39:09 +0300

    installed react router dom

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 );
 
 
