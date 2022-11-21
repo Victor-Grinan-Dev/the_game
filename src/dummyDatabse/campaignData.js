e8785803737f67b2282ac647e6cd8aa4aae1e8bf
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-21 12:00:38 +0200

    tile by sonja added

--- src/dummyDatabse/campaignData.js
+++ src/dummyDatabse/campaignData.js
@@ -4,3 +4,4 @@ import { allFactions } from "./factions";
 
 export const theBattleForTheChorizo = new Campaign("test_campaign", "The Batlle for the chorizo", 1000, hexTestMap, allFactions, 4, 'weeks');
 
+//
